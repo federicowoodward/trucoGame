630ef11fadb53ff5474507b990672cf31076a200
federicowoodward <83924684+federicowoodward@users.noreply.github.com>
2022-03-03 12:44:27 -0300

    Add files via upload

--- js/trucogame.js
+++ js/trucogame.js
@@ -50,26 +50,63 @@ const cuatroC = new Carta(4,"copa",1,false);
 let allCards = [macho,hembra,sieteFuerteE,sieteFuerteO,tresE,tresB,tresO,tresC,dosE,dosB,dosO,dosC,falsoO,falsoC,doceE,doceB,doceO,doceC,onceE,onceB,onceO,onceC,sieteFalsoB,sieteFalsoC,seisE,
     seisB,seisO,seisC,cincoE,cincoB,cincoO,cincoC,cuatroE,cuatroB,cuatroO,cuatroC];
 
+let boton = document.getElementById("btnEvento"); 
+
+let botonC1 = document.getElementById("btnCarta1");
+let botonC2 = document.getElementById("btnCarta2");
+let botonC3 = document.getElementById("btnCarta3");
+
+let pcCards = [];
+
+let playerCards = [];
+
+let mesaDeJuego = [];
+
+let nameTagTab = document.getElementById("nameTagTab");
+let bienvenidoName = document.getElementById("bienvenidoName");
+
 // ---------------------------------------    eventos --------------------------------
-let boton = document.getElementById("btnEvento");
+
 boton.addEventListener("click", startGame);
+
+botonC1.addEventListener("click",jugada1);
+botonC2.addEventListener("click",jugada2);
+botonC3.addEventListener("click",jugada3);
+
+document.addEventListener("DOMContentLoaded",nameTag);
+
+// ------------------------------------------- funciones --------------------------------  
+function nameTag(){
+    let nameTag = prompt("Hola! Como es tu nombre?")
+    nameTagTab.innerHTML = nameTag;
+    bienvenidoName.innerHTML = "Bienvenido " + nameTag + "!!";
+    if (nameTag == []) { 
+        alert("Necesitamos tu nombre!");
+        repetirNameTag();
+    }
+}
+function repetirNameTag(){
+    nameTag();
+}
 
 function startGame () {
     mezclarCartas(allCards);
 }
 
-
-// ------------------------------------------- funciones --------------------------------  
-
 function mezclarCartas(array) {
-    let carta1 = array[Math.floor(Math.random() * array.length)];   //me da una carta random
-    if (carta1.random === true) {                                   //comprueba q la carta no haya sido ya elegida                    
-        mezclarCartas(allCards);                                    //si ya se elijio reejecuta
-    } else if (carta1.random === false) {                           //comprueba q la carta no haya sido ya elegida 
-        carta1.random = true;                                       //avisa q la carta fue elegida                            
-        document.querySelector(".b1").innerHTML = carta1.simbolo;   // muestra carta
-        document.querySelector(".c1").innerHTML = carta1.numero;    // muestra carta
-    }                                                               // repetir para dos cartas mas. 
+    
+    let carta1 = array[Math.floor(Math.random() * array.length)];   //me da una carta random.
+    if (carta1.random === true) {                                   //comprueba q la carta no haya sido ya elegida.                
+        mezclarCartas(allCards);                                    //si ya se elijio reejecuta.
+    } else if (carta1.random === false) {                           //comprueba q la carta no haya sido ya elegida. 
+        carta1.random = true;                                       //avisa q la carta fue elegida.                           
+        document.querySelector(".b1").innerHTML = carta1.simbolo;   // muestra carta.
+        document.querySelector(".c1").innerHTML = carta1.numero;    // muestra carta.
+        playerCards.push(carta1);                                   // añade la nueva carta al array del jugador.
+    }  
+
+    // repetir para dos cartas mas. 
+
     let carta2 = array[Math.floor(Math.random() * array.length)];
     if (carta2.random === true) {
         mezclarCartas(allCards);
@@ -77,7 +114,9 @@ function mezclarCartas(array) {
         carta2.random = true;
         document.querySelector(".b2").innerHTML = carta2.simbolo;
         document.querySelector(".c2").innerHTML = carta2.numero;
+        playerCards.push(carta2);  
     }
+
     let carta3 = array[Math.floor(Math.random() * array.length)];
     if (carta3.random === true) {
         mezclarCartas(allCards);
@@ -85,9 +124,66 @@ function mezclarCartas(array) {
         carta3.random = true;
         document.querySelector(".b3").innerHTML = carta3.simbolo;
         document.querySelector(".c3").innerHTML = carta3.numero;
+        playerCards.push(carta3);  
     }
     
+    // ahora vamos a darle cartas a la pc.
+
+    let cartaPc1 = array[Math.floor(Math.random() * array.length)];
+    if (cartaPc1.random === true) {
+        mezclarCartas(allCards);
+    } else if (cartaPc1.random === false) {
+        cartaPc1.random = true;
+        pcCards.push(cartaPc1);
+    }
+
+    let cartaPc2 = array[Math.floor(Math.random() * array.length)];
+    if (cartaPc2.random === true) {
+        mezclarCartas(allCards);
+    } else if (cartaPc2.random === false) {
+        cartaPc2.random = true;
+        pcCards.push(cartaPc2);
+    }
+
+    let cartaPc3 = array[Math.floor(Math.random() * array.length)];
+    if (cartaPc3.random === true) {
+        mezclarCartas(allCards);
+    } else if (cartaPc3.random === false) {
+        cartaPc3.random = true;
+        pcCards.push(cartaPc3);
+    }
+
+    console.log(pcCards);
+    ocultarBoton();
+    
 }
+    function ocultarBoton() {
+        document.getElementById("btnEvento").style.display = "none";
+    }
+
+    function jugada1() {
+        mesaDeJuego.push(playerCards[0]);
+        endGame();
+    }
+
+    function jugada2() {
+        mesaDeJuego.push(playerCards[1]);
+        endGame();
+    }
+
+    function jugada3() {
+        mesaDeJuego.push(playerCards[2]);
+        endGame();
+    }
+
+    function endGame() {
+        if (mesaDeJuego.length === 3) {
+        document.getElementById("botones123").style.display = "none";
+        } 
+    }
+
+    //tabla 
+
 
 function comparacionDeCarta (carta1, carta2) {
     if (carta1.peso > carta2.peso) {
@@ -101,33 +197,33 @@ function comparacionDeCarta (carta1, carta2) {
     }
 }
 
-// empezar juego
+
 
 
 
 // cantos de truco 
 let jugadorCanta = 0;
-function cantoTruco (respuesta) {
-    if (respuesta === "Quiero") {
-        return true;
-    } else {
-        "Termina la ronda, gana" + jugadorCanta;
+    function cantoTruco (respuesta) {
+        if (respuesta === "Quiero") {
+            return true;
+        } else {
+            "Termina la ronda, gana" + jugadorCanta;
+        }
     }
-}
-function cantoReTruco (jugadorCanta, respuesta) {
-    if (respuesta === "Quiero") {
-        return true;
-    } else {
-        "Termina la ronda, gana" + jugadorCanta;
+    function cantoReTruco (jugadorCanta, respuesta) {
+        if (respuesta === "Quiero") {
+            return true;
+        } else {
+            "Termina la ronda, gana" + jugadorCanta;
+        }
     }
-}
-function cantoVale4 (jugadorCanta, respuesta) {
-    if (respuesta === "Quiero") {
-        return true;
-    } else {
-        "Termina la ronda, gana" + jugadorCanta;
+    function cantoVale4 (jugadorCanta, respuesta) {
+        if (respuesta === "Quiero") {
+            return true;
+        } else {
+            "Termina la ronda, gana" + jugadorCanta;
+        }
     }
-}
 
 // nivel de canto de truco 
 
